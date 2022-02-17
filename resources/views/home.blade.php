a5d4cfaf359a77c41e218f4ca1ff338dd2217f50
AndrianiClaudio <claudioandriani1994@gmail.com>
2022-02-17 15:09:12 +0100

    Creata home e model Movie

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+@extends('guest.layout.base')
+
+@section('PageTitle')
+    HomePage
+@endsection
+
+@section('Content')
+    <div class="container">
+      HomePage
+    </div>
+@endsection
+
+
+{{-- 
+  Creato model Movie
+  
+  --}}
