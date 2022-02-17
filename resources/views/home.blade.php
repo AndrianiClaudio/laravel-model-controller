4de826a642aa1c0f9fc6087a82baa2ec487c51c8
AndrianiClaudio <claudioandriani1994@gmail.com>
2022-02-17 17:01:12 +0100

    Layout stilato con sass

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,18 +6,29 @@
 
 @section('Content')
     <div class="container">
+      <h1>Movies</h1>
+
       {{-- ci vengono passati i dati da PageController/index --}}
       {{-- {{$movies}} --}}
       @foreach ($movies as $movie)
       <div class="card">
-        <h2>{{$movie->title}}</h2>
-        <h3>{{$movie->original_title}}</h3>
+        {{-- TITOLO --}}
+        <div class="title">
+          <h2>{{$movie->title}}</h2>
+        </div>
+        {{-- TITOLO ORIGINALE --}}
+        <div class="original-title">
+          <h3><em>{{$movie->original_title}}</em></h3>
+        </div>
+        {{-- NAZIONALITA --}}
         <div class="nationality">
           <b>Nationality: </b><span>{{$movie->nationality}}</span> 
         </div>
+        {{-- DATA DI USCITA --}}
         <div class="date">
           <b>Release date: </b><span>{{$movie->date}}</span>
         </div>
+        {{-- VOTO --}}
         <div class="vote">
           <b>Vote: </b><span>{{$movie->vote}}</span>
         </div>
